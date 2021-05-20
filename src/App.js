e12590a4666da27a96a6ffd0110fc49e06c4c5a5
David-Buch <davidbuchgraber@gmail.com>
2021-05-20 12:03:53 +0200

    gif fix

--- src/App.js
+++ src/App.js
@@ -23,26 +23,28 @@ export default function App() {
     });
     return (
         <UserContext.Provider value={{ userStore, setUserStore }}>
-            {userStore.isLoading ?
-                (<Loading />) :
-                (
-                    <div>
-                        {!userStore.isLoggedIn ? (
-                            <Router>
-                                <Route exact path='/' component={SignIn} />
-                                <Route path='/signup' component={SignUp} />
-                            </Router >
+            <AlertContext.Provider value={{ alert, setAlert }}>
+                {userStore.isLoading ?
+                    (<Loading />) :
+                    (
+                        <div>
+                            {!userStore.isLoggedIn ? (
+                                <Router>
+                                    <Route exact path='/' component={SignIn} />
+                                    <Route path='/signup' component={SignUp} />
+                                </Router >
 
-                        ) : (
-                            <AlertContext.Provider value={{ alert, setAlert }}>
+                            ) : (
+
                                 <div style={{ height: '100vH', position: 'fixed', background: '#311d3f' }}>
                                     <Drawer />
                                 </div>
-                            </AlertContext.Provider>
-                        )}
-                    </div>
-                )}
-        </UserContext.Provider>
+
+                            )}
+                        </div>
+                    )}
+            </AlertContext.Provider>
+        </UserContext.Provider >
     );
 }
 
